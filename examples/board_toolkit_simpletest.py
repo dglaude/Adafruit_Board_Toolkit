4ec2470299ff3f4d5576dc1fe40813d510d13b0d
David Glaude <dglaude@users.noreply.github.com>
2025-12-17 23:02:21 +0100

    Example: Make it clear it is the REPL not found
    There could be a CircuitPython board connected, just that the REPL is not exposed.
    Previous message could be misleading.

--- examples/board_toolkit_simpletest.py
+++ examples/board_toolkit_simpletest.py
@@ -6,7 +6,7 @@ import adafruit_board_toolkit.circuitpython_serial
 
 comports = adafruit_board_toolkit.circuitpython_serial.repl_comports()
 if not comports:
-    raise Exception("No CircuitPython boards found")
+    raise Exception("No CircuitPython REPL COM port found")
 
 # Print the device paths or names that connect to a REPL.
 print([comport.device for comport in comports])
